03e4d4ceda4c3ed9bdee3d72ee566c9afe048fd8
Achintha Dilesha <achintha.dilesha@gmail.com>
2021-08-30 07:22:02 +0530

    implemented categoryItem widget
    category item widget as the items to be displayed in gridview

--- lib/screens/catergories_screen.dart
+++ lib/screens/catergories_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodie/models/categories.dart';
+import 'package:foodie/widgets/category_item_widget.dart';
 
 class CategoroiesScreen extends StatelessWidget {
   const CategoroiesScreen({Key? key}) : super(key: key);
@@ -7,14 +8,20 @@ class CategoroiesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
+      padding: const EdgeInsets.symmetric(
+        vertical: 20,
+        horizontal: 20,
+      ),
       //* ----------------- sliver-gridview-delegate -----------------
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 3,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
       //* ----------------- grid-view children -----------------
       children: categories.map((categoryTile) {
-        return Text(categoryTile.title);
+        return CategoryItemWidget(categoryTile.title, categoryTile.color);
       }).toList(),
     );
   }
---
+++ lib/widgets/category_item_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CategoryItemWidget extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  CategoryItemWidget(this.title, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      child: Text(title),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            color.withOpacity(0.7),
+            color,
+          ],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
+  }
+}
